86d3da3354a0e0af253397c6f396d8ae4b381c91
acardenas <acardenas@10IBATIDES-170L.red.emelnorte.com>
2021-03-06 21:57:49 -0500

    Clase Inicial.

---
+++ gestionTareas/src/com/claseInicial/ClaseInicialProyecto.java
@@ -0,0 +1,12 @@
+package com.claseInicial;
+
+import com.pantallas.Login;
+
+public class ClaseInicialProyecto {
+
+	public static void main(String[] args) {
+		Login login = new Login();
+		login.show();
+	}
+
+}
